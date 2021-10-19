20f76f305fcae924883a57833f10a4219daba8f5
alecs000 <alecs.2004fed@gmail.com>
2021-10-19 20:23:58 +0300

    Добавьте файлы проекта.

---
+++ 188/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _188
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите а: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите c: ");
+            double c = double.Parse(Console.ReadLine());
+            double d = Math.Sqrt( b * b - 4 * a * c);
+            double x1 = 0;
+            double x2 = 0;
+            if (d>=0)
+            {
+                x1 = (-b + d) / 2 * a;
+                x2 = (-b - d) / 2 * a;
+            }
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+        }
+    }
+}
